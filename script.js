9f9c298b51423433d7048062e4ccd88d9b5dcdff
AAQUIB EQUBAL <44823431+armanaaquib@users.noreply.github.com>
2024-11-04 22:03:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+const setMinValue = () => {
+    const today = new Date();
+    today.setDate(today.getDate());
+    document.getElementById("startDate").min = today.toISOString().slice(0, 10);
+
+    const nextDay = new Date();
+    nextDay.setDate(today.getDate() + 1);
+    document.getElementById("endDate").min = nextDay.toISOString().slice(0, 10);
+}
+
+const updateEndDate = () => {
+    const startDate = document.getElementById("startDate");
+    const selectedDate = new Date(startDate.value);
+    selectedDate.setDate(selectedDate.getDate() + 1);
+
+    const endDate = document.getElementById("endDate");
+    endDate.min = selectedDate.toISOString().slice(0, 10);
+
+    selectedDate.setDate(selectedDate.getDate() + 7);
+    endDate.value = selectedDate.toISOString().slice(0, 10);
+  }
+
+document.getElementById("booking-form").addEventListener("submit", (event) => {
+    event.preventDefault();   
+    alert("Booking successful!");
+});
